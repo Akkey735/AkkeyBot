024a8047a044e61d405c37c0873420ebf92497b1
Akkey <84906515+Akkey57492@users.noreply.github.com>
2022-02-16 20:59:09 +0900

    v1.2.2

--- antitoken.py
+++ antitoken.py
@@ -3,7 +3,7 @@
 """
 MIT License
 
-Copyright (c) 2021 Akkey
+Copyright (c) 2021-2022 Akkey
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -24,26 +24,49 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import requests
 import nextcord
 import base64
-from nextcord.errors import NotFound
 import yaml
 
-client = nextcord.AutoShardedClient(shard_count=10)
+from nextcord.errors import NotFound
 
+client = nextcord.AutoShardedClient(shard_count=10)
 with open("config.yml", "r", encoding="utf8") as file:
     config = yaml.safe_load(file)
+    
+async def check_token(token) {
+	headers = {
+		"Authorization": token
+	}
+	response = requests.get("https://discord.com/api/v9/users/@me", headers=headers)
+	if response.status_code == 200:
+		return True
+	else:
+		return False
+}
 
 @client.event
 async def on_ready():
-    print("AkkeyBot(AntiToken)が起動されました。")
+    print("AkkeyBot(ServerProtect)が起動されました。")
 
 @client.event
 async def on_message(message):
     unchecked_token_split = message.content.split(".")
+    checked_response = await check_token(message.content)
     if message.content.startswith("mfa.") and len(unchecked_token_split) == 2:
+        pass
+    elif len(unchecked_token_split) == 3 and checked_response == True:
         await message.delete()
-    elif len(unchecked_token_split) == 3:
-        await message.delete()
+   	if int(len(message.mentions)) > 5:
+   		await message.delete()
+   		warning_message = await message.reply(f"<@{message.author.id}> 最大メンション数を超過しています")
+   		await asyncio.sleep(5000)
+   		await warning_message.delete()
+   	if int(len(message.role_mentions)) > 3:
+   		await message.delete()
+   		warning_message = await message.reply(f"<@{message.author.id}> 最大メンション数を超過しています")
+   		await asyncio.sleep(5000)
+   		await warning_message.delete()
 
 client.run(config["token"])
